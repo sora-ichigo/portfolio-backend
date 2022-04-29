c3234a7005d0d8de1db15c031abf538603ef94fb
igsr5 <i@gsr5.dev>
2022-04-29 23:05:03 +0900

    Add tools.go

--- Makefile
+++ Makefile
@@ -1,10 +1,17 @@
-.PHONY: build deploy
+.PHONY: sam-build deploy migrate-create
 
 S3_BUCKET=igsr5-portfolio-api-lambda-code
 OUTPUT_FILE=terraform/template.yaml
 
-build:
+# sam
+sam-build:
 	sam build
-package: build
+package: sam-build
 	sam package --s3-bucket $(S3_BUCKET) --output-template-file $(OUTPUT_FILE)
 	cd terraform && terraform apply -auto-approve
+
+# go
+migrate-create:
+	echo "$(NAME)"
+gen:
+	go generate ./...
---
+++ tools.go
@@ -0,0 +1,11 @@
+package tools
+
+import (
+	_ "github.com/golang-migrate/migrate/v4/cmd/migrate"
+	_ "github.com/volatiletech/sqlboiler/v4"
+	_ "github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-mysql"
+)
+
+//go:generate go build -v -tags 'mysql' -o=./bin/migrate github.com/golang-migrate/migrate/v4/cmd/migrate
+//go:generate go build -v -o=./bin/sqlboiler github.com/volatiletech/sqlboiler/v4
+//go:generate go build -v -o=./bin/sqlboiler-mysql github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-mysql
