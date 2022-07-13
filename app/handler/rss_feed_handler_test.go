a5e6f6da3706e16bc21838a96a649655e6295ed7
igsr5 <i@gsr5.dev>
2022-07-14 01:00:08 +0900

    Add test Helper setup

--- app/handler/rss_feed_handler_test.go
+++ app/handler/rss_feed_handler_test.go
@@ -12,7 +12,39 @@ import (
 )
 
 func TestGetRSSFeedsHeandler(t *testing.T) {
+	tests := []struct {
+		name       string
+		url        string
+		request    events.APIGatewayProxyRequest
+		statusCode int
+	}{
+		{
+			name: "get all rss feeds",
+			request: events.APIGatewayProxyRequest{
+				Headers: map[string]string{
+					"Content-Type": "application/json",
+				},
+			},
+			statusCode: 200,
+		},
+	}
+
+	app, _ := setup(t)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res, err := app.RSSFeedHandler.GetRSSFeeds(test.request)
+			if err != nil {
+				t.Fatalf("failed to RSSFeedHandler.GetRSSFeeds(). %v", err)
+			}
+
+			if res.StatusCode != test.statusCode {
+				t.Fatalf("bad status code by RSSFeedHandler.GetRSSFeeds(). got: %d, want: %d", res.StatusCode, test.statusCode)
+			}
+		})
+	}
 }
+
 func TestPostRSSFeedsHeandler(t *testing.T) {
 	tests := []struct {
 		name            string
@@ -47,6 +79,31 @@ func TestPostRSSFeedsHeandler(t *testing.T) {
 			isCreateRssFeed: false,
 		},
 	}
+
+	app, db := setup(t)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res, err := app.RSSFeedHandler.CreateRSSFeed(test.request)
+			if err != nil {
+				t.Fatalf("failed to RSSFeedHandler.CreateRSSFeed(). %v", err)
+			}
+
+			if res.StatusCode != test.statusCode {
+				t.Fatalf("bad status code by RSSFeedHandler.CreateRSSFeed(). got: %d, want: %d", res.StatusCode, test.statusCode)
+			}
+
+			exists, _ := models.RSSFeeds(models.RSSFeedWhere.URL.EQ(test.url)).Exists(context.Background(), db)
+			if exists != test.isCreateRssFeed {
+				t.Fatalf("bad RSSFeed exists")
+			}
+		})
+	}
+}
+
+func setup(t *testing.T) (*di.App, *sql.DB) {
+	t.Helper()
+
 	db, err := repository.NewDB()
 	if err != nil {
 		t.Fatalf("failed to repository.NewDB(). %v", err)
@@ -59,23 +116,7 @@ func TestPostRSSFeedsHeandler(t *testing.T) {
 		t.Fatalf("failed to di.NewApp(). %v", err)
 	}
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			res, err := app.RSSFeedHandler.CreateRSSFeed(test.request)
-			if err != nil {
-				t.Fatalf("failed to PostRSSFeedsHandler.Invoke(). %v", err)
-			}
-
-			if res.StatusCode != test.statusCode {
-				t.Fatalf("bad status code by PostRSSFeedsHandler.Invoke(). got: %d, want: %d", res.StatusCode, test.statusCode)
-			}
-
-			exists, _ := models.RSSFeeds(models.RSSFeedWhere.URL.EQ(test.url)).Exists(context.Background(), db)
-			if exists != test.isCreateRssFeed {
-				t.Fatalf("bad RSSFeed exists")
-			}
-		})
-	}
+	return app, db
 }
 
 func deleteAllRssFeeds(t *testing.T, db *sql.DB) {
