c41c5890ad1baaa73555352897de001bbcd8c8b7
igsr5 <i@gsr5.dev>
2022-07-17 21:51:00 +0900

    Add sam definition for GET:/rss_feed/{id}

---
+++ api/get_rss_feed/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+	"portfolio-backend/app/di"
+	"portfolio-backend/lib/sentryset"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	defer sentryset.CleanUp()
+
+	app, err := di.NewApp()
+	if err != nil {
+		log.Fatalf("function failed with errors: %#v", err)
+		os.Exit(1)
+	}
+
+	lambda.Start(app.RSSFeedHandler.GetRSSFeed)
+}
