c2a1b262fee2ee6339908195ce8b0b8f0c9c42e0
igsr5 <i@gsr5.dev>
2022-07-17 21:26:57 +0900

    Implement RSSFeedRepository.GetRSSFeed

--- domain/rss_feed.go
+++ domain/rss_feed.go
@@ -16,6 +16,7 @@ type RSSFeed struct {
 
 type RSSFeedRepository interface {
 	GetRSSFeeds(ctx context.Context) ([]RSSFeed, error)
+	GetRSSFeed(ctx context.Context, id string) (*RSSFeed, error)
 	CreateRSSFeed(ctx context.Context, input rss_feeds_pb.CreateRSSFeedRequest) error
 	IsExistsUrl(ctx context.Context, url string) (bool, error)
 }
