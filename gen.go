41aba0bbb4b131e5db7c52469d5acb377e68423c
igsr5 <i@gsr5.dev>
2022-07-18 00:31:37 +0900

    Add mockgen to tools.go

--- gen.go
+++ gen.go
@@ -4,3 +4,4 @@ package tools
 
 //go:generate ./bin/sqlboiler ./bin/sqlboiler-mysql
 //go:generate ./bin/wire ./app/di
+//go:generate ./bin/mockgen -source=./domain/rss_feed.go -destination=./domain/mock/rss_feed_mock.go
--- tools.go
+++ tools.go
@@ -5,6 +5,7 @@ package tools
 import (
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/golang-migrate/migrate/v4/cmd/migrate"
+	_ "github.com/golang/mock/mockgen"
 	_ "github.com/google/wire/cmd/wire"
 	_ "github.com/volatiletech/sqlboiler/v4"
 	_ "github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-mysql"
@@ -14,3 +15,4 @@ import (
 //go:generate go build -v -o=./bin/sqlboiler github.com/volatiletech/sqlboiler/v4
 //go:generate go build -v -o=./bin/sqlboiler-mysql github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-mysql
 //go:generate go build -v -o=./bin/wire github.com/google/wire/cmd/wire
+//go:generate go build -v -o=./bin/mockgen github.com/golang/mock/mockgen
